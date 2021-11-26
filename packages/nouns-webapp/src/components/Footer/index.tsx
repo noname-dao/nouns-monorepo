8469316a1b2522dfac982a44a8189ddb8273beba
Andrew Developer <slider7259@gmail.com>
2021-11-26 22:37:12 +0300

    Update footer links

--- packages/nouns-webapp/src/components/Footer/index.tsx
+++ packages/nouns-webapp/src/components/Footer/index.tsx
@@ -1,15 +1,11 @@
 import classes from './Footer.module.css';
 import { Container } from 'react-bootstrap';
-import { buildEtherscanAddressLink } from '../../utils/etherscan';
 import { externalURL, ExternalURL } from '../../utils/externalURL';
-import config from '../../config';
 import Link from '../Link';
 
 const Footer = () => {
   const twitterURL = externalURL(ExternalURL.twitter);
   const discordURL = externalURL(ExternalURL.discord);
-  const etherscanURL = buildEtherscanAddressLink(config.addresses.nounsToken);
-  const discourseURL = externalURL(ExternalURL.discourse);
 
   return (
     <div className={classes.wrapper}>
@@ -17,8 +13,6 @@ const Footer = () => {
         <footer className={classes.footerSignature}>
           <Link text="discord" url={discordURL} leavesPage={true} />
           <Link text="twitter" url={twitterURL} leavesPage={true} />
-          <Link text="etherscan" url={etherscanURL} leavesPage={true} />
-          <Link text="discourse" url={discourseURL} leavesPage={false} />
         </footer>
       </Container>
     </div>
--- packages/nouns-webapp/src/utils/externalURL.ts
+++ packages/nouns-webapp/src/utils/externalURL.ts
@@ -2,18 +2,15 @@ export enum ExternalURL {
   discord,
   twitter,
   notion,
-  discourse,
 }
 
 export const externalURL = (externalURL: ExternalURL) => {
   switch (externalURL) {
     case ExternalURL.discord:
-      return 'http://discord.gg/nouns';
+      return 'https://discord.gg/Mmdp4dbmRh';
     case ExternalURL.twitter:
-      return 'https://twitter.com/nounsdao';
+      return 'https://twitter.com/Daononame';
     case ExternalURL.notion:
-      return 'https://nouns.notion.site/Explore-Nouns-a2a9dceeb1d54e10b9cbf3f931c2266f';
-    case ExternalURL.discourse:
-      return 'https://discourse.nouns.wtf/';
+      return 'https://nonamedao.com';
   }
 };
