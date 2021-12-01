892db4eb77b72985d0634b5e3e19ebd793e463c5
Andrew Developer <slider7259@gmail.com>
2021-12-01 15:26:46 +0300

    Change 404 page image

--- packages/nouns-webapp/src/pages/NotFound/index.tsx
+++ packages/nouns-webapp/src/pages/NotFound/index.tsx
@@ -1,13 +1,13 @@
 import { Col, Image } from 'react-bootstrap';
 import Section from '../../layout/Section';
 import classes from './NotFound.module.css';
-import _404img from '../../assets/404noun.png';
+import logo from '../../assets/logo.svg';
 
 const NotFoundPage = () => {
   return (
     <Section fullWidth={false}>
       <Col lg={4}>
-        <Image src={_404img} fluid />
+        <Image src={logo} fluid />
       </Col>
       <Col lg={8}>
         <h1 className={classes.heading}>
